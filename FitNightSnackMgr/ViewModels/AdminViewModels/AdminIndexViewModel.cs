9e20970534b8b6ad9bd75441324582fff21bb2c7
shark-ctrl <1946955309@qq.com>
2020-06-01 23:29:13 +0800

    安全优化完成

---
+++ FitNightSnackMgr/ViewModels/AdminViewModels/AdminIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitNightSnackMgr.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FitNightSnackMgr.ViewModels.AdminViewModels
+{
+    public class AdminIndexViewModel
+    {
+
+
+        public string AdminName { get; set; }
+
+        public int Admin_permission { get; set; }
+
+        public int AdminId { get; set; }
+
+    }
+}
---
+++ FitNightSnackMgr/ViewModels/AdminViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitNightSnackMgr.ViewModels.AdminViewModels
+{
+    public class AdminLoginViewModel
+    {
+        public int status { get; set; }
+       
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+
+        public int Permission { get; set; }
+
+        public string Account { get; set; }
+
+        public string Token { get; set; }
+    }
+}
